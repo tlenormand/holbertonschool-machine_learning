dba2a8c3e8bb95470a0e892c89250052329ecc6c
Matthieu GROLAUD <matthieu.grolaud@ext.verallia.com>
2023-03-21 13:40:17 +0100

    fix: append

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -21,8 +21,7 @@ def cat_matrices2D(mat1, mat2, axis=0):
     result = []
 
     if axis == 0:
-        result.append(mat1)
-        result.append(mat2)
+        result.append(mat1 + mat2)
         return _deepCopy(result)
     else:
         for i in range(len(mat1)):
