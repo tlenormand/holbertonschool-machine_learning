8abec8b6cb0c0f40f9704b32da3334c8fbb4c2b1
Matthieu GROLAUD <matthieu.grolaud@ext.verallia.com>
2023-03-21 20:44:09 +0100

    fix: code style

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -18,4 +18,6 @@ def add_arrays(arr1, arr2):
     if len(arr1) != len(arr2):
         return None
 
-    return [arr1[i] + arr2[i] for i in range(len(arr1))]
+    return [
+        arr1[i] + arr2[i] for i in range(len(arr1))
+    ]
