9fa2b96e4b1988c172d3ada4fc09062f8cef3af9
Matthieu GROLAUD <matthieu.grolaud@ext.verallia.com>
2023-03-21 16:26:18 +0100

    fix: pycodestyle

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -21,4 +21,4 @@ def np_cat(mat1, mat2, axis=0):
     Returns:
         new matrix concatenate
     """
-    return np.concatenate((mat1, mat2), axis = axis)
+    return np.concatenate((mat1, mat2), axis=axis)
