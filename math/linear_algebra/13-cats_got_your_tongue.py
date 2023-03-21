f5e2b4811d94fd1864c4672bdee230bd9ebefcde
Matthieu GROLAUD <matthieu.grolaud@ext.verallia.com>
2023-03-21 16:24:50 +0100

    feat: new

---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+    Functions:
+        np_cat: function that function that concatenates two matrices
+            along a specific axis
+"""
+
+
+import numpy as np
+
+
+def np_cat(mat1, mat2, axis=0):
+    """ function that function that concatenates two matrices
+        along a specific axis
+
+    Arguments:
+        mat1: first matrix
+        mat2: second matrix
+        axis: axis to concatenate
+
+    Returns:
+        new matrix concatenate
+    """
+    return np.concatenate((mat1, mat2), axis = axis)
