9768ca3b87ed36273a3474ad12db955b7405a0c1
Matthieu GROLAUD <matthieu.grolaud@ext.verallia.com>
2023-03-21 13:30:33 +0100

    fix: deepcopy and empty

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -15,12 +15,36 @@ def add_matrices2D(mat1, mat2):
     Returns:
         new matrice with the sum of each element
     """
-    if (not mat1 or not mat2 or
-            len(mat1) != len(mat2) or
+    if len(mat1) == 0 and len(mat2) == 0:
+        return []
+
+    if (len(mat1) != len(mat2) or
             len(mat1[0]) != len(mat2[0])):
         return None
 
-    return [
+    return _deepCopy([
         [mat1[i][j] + mat2[i][j] for j in range(len(mat1))]
         for i in range(len(mat1[0]))
-    ]
+    ])
+
+
+def _deepCopy(arr):
+    """ function that deep copy an array
+
+    Arguments:
+        arr: array to copy
+
+    Returns:
+        new array copy
+    """
+    if isinstance(arr, list):
+        result = []
+
+        for i in arr:
+            result.append(_deepCopy(i))
+    elif isinstance(arr, (int, float, type(None), str, bool)):
+        result = arr
+    else:
+        return None
+
+    return result
