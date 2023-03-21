50c9b23361c14eb28d55096ad7b1f3513538f56b
Matthieu GROLAUD <matthieu.grolaud@ext.verallia.com>
2023-03-21 15:39:04 +0100

    fix: iteration matrices

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -22,7 +22,7 @@ def add_matrices2D(mat1, mat2):
 
     return [
         [mat1[i][j] + mat2[i][j] for j in range(len(mat1))]
-        for i in range(len(mat1[0]))
+        for i in range(len(mat2))
     ]
 
 
