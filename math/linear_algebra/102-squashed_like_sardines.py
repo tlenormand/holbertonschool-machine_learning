d391b7401dd8d32e1ca0470084419798ece4912d
Matthieu GROLAUD <matthieu.grolaud@ext.verallia.com>
2023-03-23 16:22:55 +0100

    fix: mismatching matrices with high dimension mismatching

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -31,8 +31,8 @@ def cat_matrices(mat1, mat2, axis=0):
     
     if axis > 1:
         result = [cat_matrices(mat1[i], mat2[i], axis - 1) for i in range(len(mat1))]
-        if result:
-            return result
+        if None in result:
+            return None
 
         return None
     else:
