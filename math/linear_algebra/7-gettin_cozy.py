52d56552a5ec479dbe0354ecf58072ac61a9afe4
Matthieu GROLAUD <matthieu.grolaud@ext.verallia.com>
2023-03-23 18:13:19 +0100

    fix: undo

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -6,27 +6,15 @@
 """
 
 
-matrix_shape = __import__('2-size_me_please').matrix_shape
-
-
 def cat_matrices2D(mat1, mat2, axis=0):
     """ function that concatenates two matrices along a specific axis
-
     Arguments:
         mat1: first matrice
         mat2: second matrice
         axis: axis to concatenate
-
     Returns:
         new matrix concatenate
     """
-    shape_mat1 = matrix_shape(mat1)
-    shape_mat2 = matrix_shape(mat2)
-
-    if (not mat1 or not mat2 or
-            len(shape_mat1) != len(shape_mat2)):
-        return None
-
     if axis == 0:
         if len(mat1[0]) != len(mat2[0]):
             return None
