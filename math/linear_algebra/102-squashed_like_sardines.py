9e37958f0fdb47ef76c761d8d6733dcbdce94da9
Matthieu GROLAUD <matthieu.grolaud@ext.verallia.com>
2023-03-23 19:06:10 +0100

    fix: style

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -24,15 +24,15 @@ def cat_matrices(mat1, mat2, axis=0):
     shape_mat1 = matrix_shape(mat1)
     shape_mat2 = matrix_shape(mat2)
 
+    # check dimention are identical or axis not too deep
+    if (len(shape_mat1) != len(shape_mat2) or
+            axis >= len(shape_mat1)):
+        return None
+
+    # check shape of the matrices are identical
     for i in range(len(shape_mat1)):
         if axis != i and shape_mat1[i] != shape_mat2[i]:
             return None
-
-    # check shape are identical or axis not too deep
-    if (len(shape_mat1) != len(shape_mat2) or
-            axis >= len(shape_mat1) or
-            axis != 0 and shape_mat1[0] != shape_mat2[0]):
-        return None
 
     if axis == 0:
         # if we are on the first axis, we concatenate the matrices
