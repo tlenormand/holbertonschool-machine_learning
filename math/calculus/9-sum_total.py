496a0f1902da16397363f703efe03db034af7815
Matthieu GROLAUD <matthieu.grolaud@ext.verallia.com>
2023-03-27 19:33:25 +0200

    fix: not valid

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -14,4 +14,7 @@ def summation_i_squared(n):
     Returns:
         sum of i squared
     """
+    if not isinstance(n, int) or n < 1:
+        return None
+
     return (n * (n + 1) * (2 * n + 1)) // 6
