35c74aec279e3927865dfdb7a2e8a3900363cdc2
Matthieu GROLAUD <matthieu.grolaud@ext.verallia.com>
2023-04-11 15:12:10 +0200

    fix: pycodestyle

--- supervised_learning/classification/14-neural_network.py
+++ supervised_learning/classification/14-neural_network.py
@@ -168,7 +168,7 @@ class NeuralNetwork:
 
         if alpha <= 0:
             raise ValueError("alpha must be positive")
-        
+
         for i in range(iterations):
             self.forward_prop(X)
             self.gradient_descent(X, Y, self.__A1, self.__A2, alpha)
