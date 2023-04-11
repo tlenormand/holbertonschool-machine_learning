a826c3f33e9ed3440a9616d83eb7df203af59f3f
Matthieu GROLAUD <matthieu.grolaud@ext.verallia.com>
2023-04-11 15:11:29 +0200

    fix: keepdims

--- supervised_learning/classification/14-neural_network.py
+++ supervised_learning/classification/14-neural_network.py
@@ -143,3 +143,34 @@ class NeuralNetwork:
         self.__b2 = self.__b2 - alpha * db2
         self.__W1 = self.__W1 - alpha * dW1
         self.__b1 = self.__b1 - alpha * db1
+
+    def train(self, X, Y, iterations=5000, alpha=0.05):
+        """ Trains the neural network
+
+        Arguments:
+            X: input data
+            Y: correct labels for the input data
+            iterations: number of iterations to train over
+            alpha: learning rate
+
+        Returns:
+            The evaluation of the training data after iterations of training
+            have occurred
+        """
+        if not isinstance(iterations, int):
+            raise TypeError("iterations must be an integer")
+
+        if iterations < 1:
+            raise ValueError("iterations must be a positive integer")
+
+        if not isinstance(alpha, float):
+            raise TypeError("alpha must be a float")
+
+        if alpha <= 0:
+            raise ValueError("alpha must be positive")
+        
+        for i in range(iterations):
+            self.forward_prop(X)
+            self.gradient_descent(X, Y, self.__A1, self.__A2, alpha)
+
+        return self.evaluate(X, Y)
