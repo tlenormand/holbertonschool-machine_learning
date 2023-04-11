fix: dW1 formula

supervised_learning/classification/13-neural_network.py
@@ -138,7 +138,7 @@ class NeuralNetwork:
         self.__b2 = self.__b2 - alpha * db2
 
         dZ1 = np.matmul(self.__W2.T, dZ2) * (A1 * (1 - A1))
-        dW1 = 1 / m * np.matmul(dZ2, X.T)
+        dW1 = 1 / m * np.matmul(dZ1, X.T)
         db1 = 1 / m * np.sum(dZ1, axis=1)
         self.__W1 = self.__W1 - alpha * dW1
         self.__b1 = self.__b1 - alpha * db1