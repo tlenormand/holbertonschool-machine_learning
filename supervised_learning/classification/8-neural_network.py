29dcf75aa84791691a05fbb50208517d852c4986
Matthieu GROLAUD <matthieu.grolaud@ext.verallia.com>
2023-04-11 13:35:09 +0200

    fix: use np.zeros for bA

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -30,7 +30,7 @@ class NeuralNetwork:
             raise ValueError("nodes must be a positive integer")
 
         self.W1 = np.random.normal(size=(nodes, nx))
-        self.b1 = 0
+        self.b1 = np.zeros((nodes, 1))
         self.A1 = 0
         self.W2 = np.random.normal(size=(1, nodes))
         self.b2 = 0
