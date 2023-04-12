cdcb0a440eb4a1efcc62f6afec713533a846376e
Matthieu GROLAUD <matthieu.grolaud@ext.verallia.com>
2023-04-12 18:25:09 +0200

    feat: new

---
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" Module that contains the function one_hot_encode """
+
+import numpy as np
+
+
+def one_hot_encode(Y, classes):
+    """ Function that converts a numeric label vector into a one-hot matrix
+
+    Arguments:
+        Y: numpy.ndarray with shape (m,) containing numeric class labels
+        classes: maximum number of classes found in Y
+
+    Returns:
+        A one-hot encoding of Y with shape (classes, m), or None on failure
+    """
+    if not isinstance(Y, (list, np.ndarray)) or len(Y) == 0:
+        return None
+
+    array = np.zeros((classes, Y.size))
+
+    for i in range(len(array[0])):
+        array[Y[i]][i] = 1
+
+    return array
