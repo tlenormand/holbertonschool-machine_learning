9e47b4eaef7fb6750c31c6a3bc5d891d4b398de0
Matthieu GROLAUD <matthieu.grolaud@ext.verallia.com>
2023-04-12 18:42:46 +0200

    feat: new

---
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" Module that contains the function one_hot_decode """
+
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """ Function that converts a one-hot matrix into a vector of labels
+
+    Arguments:
+        one_hot: one-hot encoded numpy.ndarray with shape (classes, m)
+
+    Returns:
+        A numpy.ndarray with shape (m, ) containing the numeric labels for each
+        example, or None on failure
+    """
+    if not isinstance(one_hot, np.ndarray) or len(one_hot) == 0:
+        return None
+
+    # argmax returns the indices of the maximum values along an axis
+    return np.argmax(one_hot, axis=0)
