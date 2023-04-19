7919891b2eb963ae2c39a55e68f1a4b344bf028d
Matthieu GROLAUD <matthieu.grolaud@ext.verallia.com>
2023-04-19 15:44:26 +0200

    fix: pycodestyle

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -14,7 +14,8 @@ def create_layer(prev, n, activation):
         activation: the activation function that the layer should use
     Return: the tensor output of the layer
     """
-    layer_weights = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    layer_weights = \
+        tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
 
     layer = tf.layers.Dense(
         n,
