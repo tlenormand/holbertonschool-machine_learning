3c4cf7dd68eb99febb4ad47e8ef7c2c38b7d84fb
Matthieu GROLAUD <matthieu.grolaud@ext.verallia.com>
2023-04-24 17:12:27 +0200

    fix: code

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -1,35 +1,38 @@
 #!/usr/bin/env python3
-"""
-module containing function train_mini_batch
-"""
-import tensorflow as tf
+""" Mini-Batch """
+
+import tensorflow.compat.v1 as tf
 
 shuffle_data = __import__('2-shuffle_data').shuffle_data
 
 
-def train_mini_batch(X_train, Y_train, X_valid, Y_valid,
+def train_mini_batch(X_train, Y_train,
+                     X_valid, Y_valid,
                      batch_size=32, epochs=5,
                      load_path="/tmp/model.ckpt", save_path="/tmp/model.ckpt"):
-    """
-    function that trains a loaded neural network model
-        using mini-batch gradient descent
-    Args:
-        X_train: numpy.ndarray of shape (m, 784)
-            containing the training input data
-            with m: the number of data points
-                784: the number of input features
-        Y_train: one-hot numpy.ndarray of shape (m, 10)
-            containing the training labels
-            with: 10 is the number of classes the model should classify
-        X_valid: numpy.ndarray of shape (m, 784) containing the validation data
-        Y_valid: one-hot numpy.ndarray of shape (m, 10)
+    """ Trains a loaded neural network model using mini-batch gradient descent
+
+    Argumets:
+        X_train is a numpy.ndarray of shape (m, 784)
+                containing the training data
+            * m is the number of data points
+            * 784 is the number of input features
+        Y_train is a one-hot numpy.ndarray of shape (m, 10)
+                containing the training labels
+            * m is the number of data points
+            * 10 is the number of classes the model should classify
+        X_valid is a numpy.ndarray of shape (m, 784)
+            containing the validation data
+        Y_valid is a one-hot numpy.ndarray of shape (m, 10)
             containing the validation labels
-        batch_size: the number of data points in a batch
-        epochs: the number of times the training should pass through
-            the whole dataset
-        load_path: the path from which to load the model
-        save_path: the path to where the model should be saved after training
-    Return: the path where the model was saved
+        batch_size is the number of data points in a batch
+        epochs is the number of times the training should
+            pass through the whole dataset
+        load_path is the path from which to load the model
+        save_path is the path to where the model should be saved after training
+
+    Returns:
+        The path where the model was saved
     """
     # meta graph and restore session
     with tf.Session() as session:
@@ -37,31 +40,30 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid,
         saver.restore(session, load_path)
 
         # Get the following tensors and ops from the collection restored
-        x = tf.get_collection('x')[0]
-        y = tf.get_collection('y')[0]
-        accuracy = tf.get_collection('accuracy')[0]
-        loss = tf.get_collection('loss')[0]
-        train_op = tf.get_collection('train_op')[0]
+        x = tf.get_collection("x")[0]
+        y = tf.get_collection("y")[0]
 
-        # loop over epochs
-        if X_train.shape[0] % batch_size == 0:
-            iterations = X_train.shape[0] // batch_size
-        else:
-            iterations = (X_train.shape[0] // batch_size) + 1
+        accuracy = tf.get_collection("accuracy")[0]
+        loss = tf.get_collection("loss")[0]
+        train_op = tf.get_collection("train_op")[0]
 
+        m = X_train.shape[0]
+        batches = m // batch_size
+
+        # Loop over epochs
         for epoch in range(epochs + 1):
             training_cost = session.run(
-                loss,
-                feed_dict={x: X_train, y: Y_train})
+                loss, feed_dict={x: X_train, y: Y_train}
+            )
             training_accuracy = session.run(
-                accuracy,
-                feed_dict={x: X_train, y: Y_train})
+                accuracy, feed_dict={x: X_train, y: Y_train}
+            )
             validation_cost = session.run(
-                loss,
-                feed_dict={x: X_valid, y: Y_valid})
+                loss, feed_dict={x: X_valid, y: Y_valid}
+            )
             validation_accuracy = session.run(
-                accuracy,
-                feed_dict={x: X_valid, y: Y_valid})
+                accuracy, feed_dict={x: X_valid, y: Y_valid}
+            )
 
             print("After {} epochs:".format(epoch))
             print("\tTraining Cost: {}".format(training_cost))
@@ -69,39 +71,31 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid,
             print("\tValidation Cost: {}".format(validation_cost))
             print("\tValidation Accuracy: {}".format(validation_accuracy))
 
-            # shuffle data
             shuffled_X, shuffled_Y = shuffle_data(X_train, Y_train)
 
-            # loop over the batches
-            if epoch < epochs:
-
-                start = 0
-                if batch_size < X_train.shape[0]:
-                    end = batch_size
-                else:
+            # Loop over batches
+            for batch in range(batches + 1):
+                start = batch * batch_size
+                end = start + batch_size
+                if end > X_train.shape[0]:
                     end = X_train.shape[0]
 
-                for step_number in range(iterations):
+                X_batch = shuffled_X[start:end]
+                Y_batch = shuffled_Y[start:end]
 
-                    X_batch = shuffled_X[start:end]
-                    Y_batch = shuffled_Y[start:end]
-                    session.run(train_op, feed_dict={x: X_batch, y: Y_batch})
+                session.run(train_op, feed_dict={x: X_batch, y: Y_batch})
 
-                    start += batch_size
-                    if end + batch_size < X_train.shape[0]:
-                        end += batch_size
-                    else:
-                        end = X_train.shape[0]
+                if batch % 100 == 0 and batch != 0:
+                    step_cost = session.run(
+                        loss, feed_dict={x: X_batch, y: Y_batch}
+                    )
+                    step_accuracy = session.run(
+                        accuracy, feed_dict={x: X_batch, y: Y_batch}
+                    )
 
-                    if step_number > 0 and step_number % 100 == 0:
-                        step_cost = session.run(
-                            loss, feed_dict={x: X_batch, y: Y_batch})
-                        step_accuracy = session.run(
-                            accuracy, feed_dict={x: X_batch, y: Y_batch})
-                        print("\tStep {}:".format(step_number))
-                        print("\t\tCost: {}".format(step_cost))
-                        print("\t\tAccuracy: {}".format(step_accuracy))
+                    print("\tStep {}:".format(batch))
+                    print("\t\tCost: {}".format(step_cost))
+                    print("\t\tAccuracy: {}".format(step_accuracy))
 
-        # save session
-        save_path = saver.save(session, save_path)
-        return save_path
+    saver.save(session, save_path)
+    return save_path
