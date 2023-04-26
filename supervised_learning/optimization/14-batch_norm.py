b35c8a996c8282ba92dcc1a23b7c1bf88a76bb1c
Matthieu GROLAUD <matthieu.grolaud@ext.verallia.com>
2023-04-26 14:39:14 +0200

    feat: new

---
+++ supervised_learning/optimization/14-batch_norm.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+""" Batch Normalization """
+
+import tensorflow.compat.v1 as tf
+
+
+def create_batch_norm_layer(prev, n, activation):
+    """ Creates a batch normalization layer for a neural network in tensorflow.
+
+        Args:
+            prev: (tf.Tensor) containing the output of the previous layer.
+            n: (int) containing the number of nodes in the layer to be created.
+            activation: (tf.Operation) containing the activation function that
+                        should be used on the output of the layer.
+
+        Returns:
+            A tensor of the activated output for the layer.
+    """
+    initializer = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+
+    layer = tf.layers.Dense(
+        units=n,
+        kernel_initializer=initializer,
+        activation=None
+    )
+
+    Z = layer(prev)
+
+    mean, variance = tf.nn.moments(Z, axes=[0])
+    gamma = tf.Variable(tf.ones([n]), name='gamma')
+    beta = tf.Variable(tf.zeros([n]), name='beta')
+
+    Z_norm = tf.nn.batch_normalization(
+        x=Z,
+        mean=mean,
+        variance=variance,
+        offset=beta,
+        scale=gamma,
+        variance_epsilon=1e-8
+    )
+
+    return activation(Z_norm)
