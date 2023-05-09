d1fc8fa3722276aaa00b0a9e180e18fcf949ade6
Matthieu GROLAUD <matthieu.grolaud@ext.verallia.com>
2023-05-09 22:43:30 +0200

    fix: early_stopping

--- supervised_learning/keras/7-train.py
+++ supervised_learning/keras/7-train.py
@@ -1,46 +1,46 @@
 #!/usr/bin/env python3
-""" Train """
+
+"""Useless comments"""
 
 import tensorflow.keras as K
 
 
 def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, early_stopping=False,
-                patience=0, learning_rate_decay=False, alpha=0.1,
-                decay_rate=1, verbose=True, shuffle=False):
-    """ Function that trains a model using mini-batch gradient descent
-
-    Arguments:
-        network: model to train
-        data: numpy.ndarray of shape (m, nx) containing the input data
-        labels: one-hot numpy.ndarray of shape (m, classes) containing
-            the labels of data
-        batch_size: size of the batch used for mini-batch gradient descent
-        epochs: number of passes through data for mini-batch gradient descent
-        verbose: boolean that determines if output should be printed during
-            training
-        shuffle: boolean that determines whether to shuffle the batches every
-            epoch
-
-    Returns:
-        The History object generated after training the model
+                patience=0, learning_rate_decay=False,
+                alpha=0.1, decay_rate=1, verbose=True,
+                shuffle=False):
     """
-    # callbacks refers to a list of callbacks to apply during training
-    # ref: https://keras.io/api/models/model_training_apis/#fit-method
-    callbacks = []
+    Train a model using mini-batch gradient descente
+    :param network: The model to perform the fit on
+    :param data: The dataset
+    :param labels: The thruth labels
+    :param batch_size: The batch size
+    :param epochs: The number of epoch to perform
+    :param validation_data: The validation dataset (Could not exist)
+    :param early_stopping: If we want an early stopping
+    :param patience: The patience in case of early stop
+    :param learning_rate_decay: The learning rate decay used for the
+                                 callback
+    :param alpha: The initial learning_rate
+    :param decay_rate: The decay rate
+    :param verbose: The level of verbose (True or False)
+    :param shuffle: If the data need to be shuffle before each epoach
+    :return: The fited model
+    """
+    def lr_decay(epochs):
+        return alpha / (1 + decay_rate * epochs)
+
+    callback_list = []
 
     if learning_rate_decay and validation_data:
-        lr_decay = K.callbacks.LearningRateScheduler(
-            schedule=alpha / (1 + decay_rate * epochs),
-            verbose=1
-        )
-        callbacks.append(lr_decay)
+        callback_list.append(K.callbacks.LearningRateScheduler(
+            lr_decay,
+            verbose=True
+    ))
 
     if early_stopping and validation_data:
-        early_stopping = K.callbacks.EarlyStopping(
-            patience=patience
-        )
-        callbacks.append(early_stopping)
+        callback_list.append(K.callbacks.EarlyStopping(patience=patience))
 
     return network.fit(
         x=data,
@@ -50,5 +50,5 @@ def train_model(network, data, labels, batch_size, epochs,
         verbose=verbose,
         shuffle=shuffle,
         validation_data=validation_data,
-        callbacks=callbacks
+        callbacks=callback_list
     )
