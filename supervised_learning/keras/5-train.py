78e3e84035e81b6eff37b7a9fa0c352d810b6e65
Matthieu GROLAUD <matthieu.grolaud@ext.verallia.com>
2023-05-09 22:35:26 +0200

    fix: checker

--- supervised_learning/keras/5-train.py
+++ supervised_learning/keras/5-train.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """ Train """
 
+import tensorflow.keras as K
+
 
 def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, verbose=True, shuffle=False):
