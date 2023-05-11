57823977114f1d23964ca82de284c8ed40d2e9c6
Matthieu GROLAUD <matthieu.grolaud@ext.verallia.com>
2023-05-11 16:09:31 +0200

    fix: correct output

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -21,53 +21,35 @@ def convolve_grayscale_same(images, kernel):
     Returns:
         a numpy.ndarray containing the convolved images
     """
-    def add_padding(images):
-        """ add padding to the image """
-        if kernel_heigh % 2:
-            padding_heigh = int((kernel_heigh - 1) / 2)
-        else:
-            padding_heigh = int(kernel_heigh / 2)
-
-        if kernel_width % 2:
-            padding_width = int((kernel_width - 1) / 2)
-        else:
-            padding_width = int(kernel_width / 2)
-
-        return np.pad(
-            images,
-            # ((before height, after height), (before width, after width))
-            pad_width=(
-                (0, 0),
-                (padding_heigh, padding_heigh),
-                (padding_width, padding_width)
-            ),
-            mode='constant'
-        )
-
-    # add padding to the image in order to keep the same size
-    images = add_padding(images)
-
     nb_image, image_heigh, image_width = images.shape
     kernel_heigh, kernel_width = kernel.shape
 
     # output size
-    conv_heigh = image_heigh - kernel_heigh + 1
-    conv_width = image_width - kernel_width + 1
+    output_heigh = int(image_heigh)
+    output_width = int(image_width)
 
-    conv = np.zeros((nb_image, conv_heigh, conv_width))
-    for heigh in range(conv_heigh):
-        for width in range(conv_width):
-            # every image
-            # height from i to kernel height + i
-            # width from j to kernel width + j
-            image = images[
-                :,
-                heigh:heigh + kernel_heigh,
-                width:width + kernel_width
-            ]
-            # kernel contains operations for every image
-            # axis if for the image
-            # 0 the number of images, 1 the height, 2 the width
-            conv[:, heigh, width] = np.sum(image * kernel, axis=(1, 2))
+    # convolution output
+    conv = np.zeros((nb_image, output_heigh, output_width))
+
+    # Add padding to the images
+    pad_height = int(kernel_heigh / 2)
+    pad_width = int(kernel_width / 2)
+    images = np.pad(
+        images,
+        # ((before height, after height), (before width, after width))
+        pad_width=((0, 0), (pad_height, pad_height), (pad_width, pad_width)),
+        mode='constant'
+    )
+
+    for height in range(output_heigh):
+        for width in range(output_width):
+            conv[:, height, width] = np.sum(
+                images[
+                    :,
+                    height: height + kernel_heigh,
+                    width: width + kernel_width
+                ] * kernel,
+                axis=(1, 2)
+            )
 
     return conv
