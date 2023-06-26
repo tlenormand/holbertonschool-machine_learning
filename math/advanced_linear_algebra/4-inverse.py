93d5c733f4b2a6f9cc005fc3a8c90b3fc404e4ac
tlenormand <lenormand.thomas76110@gmail.com>
2023-06-26 16:32:00 +0200

    feat: new

---
+++ math/advanced_linear_algebra/4-inverse.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+""" Inverse """
+
+determinant = __import__('0-determinant').determinant
+cofactor = __import__('2-cofactor').cofactor
+
+
+def inverse(matrix):
+    """ calculates the inverse of a matrix
+
+    Arguments:
+        matrix (list): matrix to calculate
+
+    Returns:
+        the inverse of matrix
+    """
+    if type(matrix) is not list or len(matrix) == 0:
+        raise TypeError('matrix must be a list of lists')
+    for row in matrix:
+        if type(row) is not list:
+            raise TypeError('matrix must be a list of lists')
+    for row in matrix:
+        if len(row) != len(matrix):
+            raise ValueError('matrix must be a non-empty square matrix')
+
+    if len(matrix) == 1:
+        return [[1 / matrix[0][0]]]
+
+    det = determinant(matrix)
+    if det == 0:
+        return None
+
+    if len(matrix) == 2:
+        return [[matrix[1][1] / det, -1 * matrix[0][1] / det],
+                [-1 * matrix[1][0] / det, matrix[0][0] / det]]
+
+    cofactorMatrix = cofactor(matrix)
+    inverse = []
+    for i in range(len(cofactorMatrix)):
+        inverse.append([])
+        for j in range(len(cofactorMatrix)):
+            inverse[i].append(cofactorMatrix[i][j] / det)
+
+    return inverse
---
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" Definiteness """
+
+import numpy as np
+
+
+def definiteness(matrix):
+    """ calculates the definiteness of a matrix
+
+    Arguments:
+        matrix (list): matrix to calculate
+
+    Returns:
+        the definiteness of matrix
+    """
+    if type(matrix) is not np.ndarray:
+        raise TypeError('matrix must be a numpy.ndarray')
+    if len(matrix.shape) != 2 or matrix.shape[0] != matrix.shape[1]:
+        return None
+
+    eigenvalues = np.linalg.eigvals(matrix)
+    if all(eigenvalues > 0):
+        return 'Positive definite'
+    if all(eigenvalues >= 0):
+        return 'Positive semi-definite'
+    if all(eigenvalues < 0):
+        return 'Negative definite'
+    if all(eigenvalues <= 0):
+        return 'Negative semi-definite'
+
+    return 'Indefinite'
