fe52f0debe7ec7bd1eb0ae817ef87979e494eb78
tlenormand <lenormand.thomas76110@gmail.com>
2023-06-26 16:11:41 +0200

    feat: new

---
+++ math/advanced_linear_algebra/2-cofactor.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+""" Cofactor """
+
+determinant = __import__('0-determinant').determinant
+minor = __import__('1-minor').minor
+
+
+def cofactor(matrix):
+    """ calculates the cofactor matrix of a matrix
+
+    Arguments:
+        matrix (list): matrix to calculate
+
+    Returns:
+        the cofactor matrix of matrix
+    """
+    if type(matrix) is not list or len(matrix) == 0:
+        raise TypeError('matrix must be a list of lists')
+    for row in matrix:
+        if type(row) is not list:
+            raise TypeError('matrix must be a list of lists')
+    for row in matrix:
+        if len(row) != len(matrix):
+            raise ValueError('matrix must be a non-empty square matrix')
+
+    if len(matrix) == 1:
+        return [[1]]
+
+    minorMatrix = minor(matrix)
+
+    sign = 1
+    cofactor = []
+    for i in range(len(minorMatrix)):
+        cofactor.append([])
+        for j in range(len(minorMatrix)):
+            cofactor[i].append(sign * minorMatrix[i][j])
+            sign *= -1
+
+    return cofactor
