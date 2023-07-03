72d16565f55db9982ed8c8c19ad1a2f9b6f1c733
tlenormand <lenormand.thomas76110@gmail.com>
2023-07-03 15:13:04 +0200

    fix: errors messages

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -65,7 +65,7 @@ def intersection(x, n, P, Pr):
         raise ValueError("x cannot be greater than n")
     if type(P) is not np.ndarray or len(P.shape) != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
-    if type(Pr) is not np.ndarray or len(Pr.shape) != 1:
+    if type(Pr) is not np.ndarray or len(Pr.shape) != 1 or Pr.shape != P.shape:
         raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
     if np.any(P > 1) or np.any(P < 0):
         raise ValueError("All values in P must be in the range [0, 1]")
@@ -73,5 +73,7 @@ def intersection(x, n, P, Pr):
         raise ValueError("All values in Pr must be in the range [0, 1]")
     if P.shape != Pr.shape:
         raise TypeError("Pr must have the same shape as P")
+    if not np.isclose(np.sum(Pr), 1):
+        raise ValueError("Pr must sum to 1")
 
     return likelihood(x, n, P) * Pr
