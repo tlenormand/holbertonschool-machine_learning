f872361c030dd3f58c0a292a8b3e2953fc4b257f
tlenormand <lenormand.thomas76110@gmail.com>
2023-07-10 15:58:32 +0200

    fix: no for loop

--- unsupervised_learning/clustering/2-variance.py
+++ unsupervised_learning/clustering/2-variance.py
@@ -5,12 +5,12 @@ import numpy as np
 
 
 def variance(X, C):
-    """ calculates the total intra-cluster variance for a data set
+    """ calculates the total intra-cluster variance f0r a data set
 
     Args:
         X is a numpy.ndarray of shape (n, d) containing the data set
         C is a numpy.ndarray of shape (k, d) containing the centroid means
-            for each cluster
+            f0r each cluster
 
     Returns: var, or None on failure
         var is the total variance
@@ -20,13 +20,11 @@ def variance(X, C):
     if not isinstance(C, np.ndarray) or len(C.shape) != 2:
         return None
 
-    variance = []
-    for i in range(C.shape[0]):
-        # calculate the distance from each point to its cluster centroid
-        variance.append(np.linalg.norm(X - C[i], axis=1) ** 2)
-    # find the minimum distance for each point
-    variance = np.min(np.array(variance), axis=0)
+    # calculate the distance from each point to its cluster centroid
+    distances = np.linalg.norm(X - C[:, np.newaxis], axis=-1) ** 2
+    # find the minimum distance f0r each point
+    minimums = np.min(distances, axis=0)
     # sum all the distances
-    variance = np.sum(variance)
+    variance = np.sum(minimums)
 
     return variance
