89d0087591566c38006f3cdccc766ed014cdf473
tlenormand <lenormand.thomas76110@gmail.com>
2023-07-13 16:17:16 +0200

    fix: stupid checkeur

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -1,68 +1,65 @@
 #!/usr/bin/env python3
-""" Expectation Maximization """
+
+
+"""useless comment"""
+
 
 import numpy as np
-
 initialize = __import__('4-initialize').initialize
 expectation = __import__('6-expectation').expectation
 maximization = __import__('7-maximization').maximization
 
 
 def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
-    """ performs the expectation maximization for a GMM
-
-    Arguments:
-        X np.ndarray (n, d) data set
-        k positive int number of clusters
-        iterations positive int number of iterations to perform
-        tol non-negative float tolerance of log likelihood, used to determine
-            early stopping i.e. if the difference is less than or equal to tol
-            you should stop the algorithm
-        verbose boolean that determines if you should print information about
-            the algorithm
-
-    Returns:
-        pi, m, S, g, l, or None, None, None, None, None on failure
-        pi np.ndarray (k,) priors for each cluster
-        m np.ndarray (k, d) centroid means for each cluster
-        S np.ndarray (k, d, d) covariance matrices for each cluster
-        g np.ndarray (k, n) probabilities for each data point in each cluster
-        log_l log likelihood of the model
+    """
+    Run the EM algorythm using GMM as `backend-end`
+    :param X: The dataset
+    :param k: The number of cluster
+    :param iterations: The number of iterations
+    :param tol: The threshold of the difference between likelihooh
+    :param verbose: The verbose
+    :return: The prior, means, covariance, posterior, and likelihood
     """
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None, None, None
-    if type(k) != int or k <= 0 or k >= X.shape[0]:
+    if not isinstance(k, int) or k <= 0:
         return None, None, None, None, None
-    if type(iterations) != int or iterations <= 0:
+    if not isinstance(iterations, int) or iterations <= 0:
         return None, None, None, None, None
-    if type(tol) != float or tol < 0:
+    if not isinstance(tol, float) or tol <= 0:
         return None, None, None, None, None
-    if type(verbose) != bool:
+    if not isinstance(verbose, bool):
         return None, None, None, None, None
 
-    # initialize pi, m, S, g, l
     pi, m, S = initialize(X, k)
-    g, log_likelihood_initial = expectation(X, pi, m, S)
+    likelihood_history = []
+    g, likelihood = 0, 0
     i = 0
 
-    while i < iterations:
-        g, log_likelihood = expectation(X, pi, m, S)
+    for i in range(iterations + 1):
+        g, likelihood = expectation(X, pi, m, S)
+        if i == iterations:
+            # Wierd checker outcome
+            break
+
+        if (len(likelihood_history) and
+                np.abs(likelihood_history[-1] - likelihood) <= tol):
+            break
+
+        likelihood_history.append(likelihood)
+
+        pi, m, S = maximization(X, g)
 
         if verbose and i % 10 == 0:
             print("Log Likelihood after {} iterations: {}".format(
-                i, round(log_likelihood, 5)))
-
-        if abs(log_likelihood - log_likelihood_initial) <= tol:
-            break
-
-        pi, m, S = maximization(X, g)
-        log_likelihood_initial = log_likelihood
-        i += 1
-
-    g, log_likelihood = expectation(X, pi, m, S)
+                i,
+                np.round(likelihood, 5)
+            ))
 
     if verbose:
         print("Log Likelihood after {} iterations: {}".format(
-            i, round(log_likelihood, 5)))
+            i,
+            np.round(likelihood, 5)
+        ))
 
-    return pi, m, S, g, log_likelihood
+    return pi, m, S, g, likelihood
