e7d1f803cde074f118ffd6a8319c8875e8400d9c
tlenormand <lenormand.thomas76110@gmail.com>
2023-07-13 14:37:06 +0200

    feat: new

---
+++ unsupervised_learning/clustering/6-expectation.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+""" Expectation """
+
+import numpy as np
+
+pdf = __import__('5-pdf').pdf
+
+
+def expectation(X, pi, m, S):
+    """ calculates the expectation step in the EM algorithm for a GMM
+
+    Arguments:
+        X np.ndarray (n, d) data set
+        pi np.ndarray (k,) priors for each cluster
+        m np.ndarray (k, d) centroid means for each cluster
+        S np.ndarray (k, d, d) covariance matrices for each cluster
+
+    Returns:
+        g, l, or None, None on failure
+        g np.ndarray (k, n) posterior probabilities for each data point
+        l total log likelihood
+    """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None
+    if not isinstance(pi, np.ndarray) or len(pi.shape) != 1:
+        return None, None
+    if not isinstance(m, np.ndarray) or len(m.shape) != 2:
+        return None, None
+    if not isinstance(S, np.ndarray) or len(S.shape) != 3:
+        return None, None
+    if (X.shape[1] != S.shape[1] or S.shape[1] != S.shape[2] or
+            X.shape[1] != m.shape[1] or pi.shape[0] != m.shape[0] or
+            pi.shape[0] != S.shape[0]):
+        return None, None
+
+    # number of data points
+    n = X.shape[0]
+    # number of clusters
+    k = pi.shape[0]
+
+    # calculate g using pdf function
+    g = np.zeros((k, n))
+    for i in range(k):
+        g[i] = pi[i] * pdf(X, m[i], S[i])
+
+    # normalize g
+    g = g / np.sum(g, axis=0)
+    # calculate l using log likelihood
+    log_l = np.sum(np.log(np.sum(g, axis=0)))
+
+    return g, log_l
