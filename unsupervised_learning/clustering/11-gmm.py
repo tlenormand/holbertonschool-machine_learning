711e5c6837f42757e561928225fef3a178f05e7d
tlenormand <lenormand.thomas76110@gmail.com>
2023-07-15 16:14:25 +0200

    fix: import

--- unsupervised_learning/clustering/11-gmm.py
+++ unsupervised_learning/clustering/11-gmm.py
@@ -1,49 +1,34 @@
 #!/usr/bin/env python3
 """ GMM """
 
-import numpy as np
-
-expectation = __import__('6-expectation').expectation
-maximization = __import__('7-maximization').maximization
+import sklearn.mixture
 
 
 def gmm(X, k):
-    """ Function that calculates a GMM from a dataset
+    """ calculates a GMM from a dataset
 
     Arguments:
-        X np.ndarray of shape (n, d) containing the dataset
-        k positive integer containing the number of clusters
+        X np.ndarray (n, d) data set
+        k positive int number of clusters
 
     Returns:
         pi, m, S, clss, bic
-        pi np.ndarray shape (k,) containing the priors for each cluster
-        m np.ndarray shape (k, d) containing the centroid means for each
-            cluster
-        S np.ndarray shape (k, d, d) containing the covariance matrices for
-            each cluster
-        clss np.ndarray shape (n,) containing the cluster indices for each
-            data point
-        bic np.ndarray shape (kmax - kmin + 1) containing the BIC value for
-            each cluster size tested
+        pi np.ndarray (k,) priors for each cluster
+        m np.ndarray (k, d) centroid means for each cluster
+        S np.ndarray (k, d, d) covariance matrices for each cluster
+        clss np.ndarray (n,) cluster indices for data points
+        bic np.ndarray (kmax - kmin + 1,) BIC value for each cluster size
     """
-    if type(X) is not np.ndarray or len(X.shape) != 2:
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None, None, None
-    if type(k) is not int or k <= 0:
+    if not isinstance(k, int) or k <= 0:
         return None, None, None, None, None
 
-    n, d = X.shape
-    pi = np.full((k,), 1 / k)
-    m = np.random.uniform(np.min(X, axis=0), np.max(X, axis=0), size=(k, d))
-    S = np.full((k, d, d), np.identity(d))
-    clss = np.zeros((n,))
-    l_prev = 0
-    tol = 1e-5
-    g, log = expectation(X, pi, m, S)
-    while abs(log - l_prev) > tol:
-        pi, m, S = maximization(X, g)
-        g, log = expectation(X, pi, m, S)
-        l_prev = log
-    clss = np.argmax(g, axis=0)
-    bic = None
+    gmm = sklearn.mixture.GaussianMixture(n_components=k).fit(X)
+    pi = gmm.weights_
+    m = gmm.means_
+    S = gmm.covariances_
+    clss = gmm.predict(X)
+    bic = gmm.bic(X)
 
     return pi, m, S, clss, bic
