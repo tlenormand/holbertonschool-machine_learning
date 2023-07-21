b929ec89debee237e9ea3c4b94052118dff8d5fe
tlenormand <lenormand.thomas76110@gmail.com>
2023-07-21 14:36:57 +0200

    fix: import

--- unsupervised_learning/hmm/4-viterbi.py
+++ unsupervised_learning/hmm/4-viterbi.py
@@ -3,7 +3,50 @@
 
 import numpy as np
 
-forward = __import__('3-forward').forward
+
+def forward(Observation, Emission, Transition, Initial):
+    """ performs the forward algorithm for a hidden markov model
+
+    Arguments:
+        Observation: np arr (T,) index of observation(s)
+        Emission: np arr (N, M) emission prob of specific observation
+            given hidden state
+        Transition: np arr (N, N) transition probabilities
+        Initial: np arr (N, 1) prob of starting in specific hidden state
+
+    Returns:
+        P, F, or None, None on failure
+            P: likelihood of the observations given the model
+            F: np arr (N, T) forward path probabilities
+    """
+    if type(Observation) is not np.ndarray or Observation.ndim != 1:
+        return None, None
+    if type(Emission) is not np.ndarray or Emission.ndim != 2:
+        return None, None
+    if type(Transition) is not np.ndarray or Transition.ndim != 2:
+        return None, None
+    if type(Initial) is not np.ndarray or Initial.ndim != 2:
+        return None, None
+
+    N, M = Emission.shape
+    if Transition.shape != (N, N):
+        return None, None
+    if Initial.shape != (N, 1):
+        return None, None
+
+    T = Observation.shape[0]
+
+    # initialize F
+    F = np.zeros((N, T))
+    F[:, 0] = Initial.T * Emission.T[Observation[0]]
+
+    for t in range(1, T):
+        F[:, t] = np.matmul(F[:, t - 1], Transition) *\
+            Emission.T[Observation[t]]
+
+    P = np.sum(F[:, T - 1])
+
+    return P, F
 
 
 def viterbi(Observation, Emission, Transition, Initial):
--- unsupervised_learning/hmm/6-baum_welch.py
+++ unsupervised_learning/hmm/6-baum_welch.py
@@ -2,8 +2,98 @@
 """HMM module"""
 import numpy as np
 
-forward = __import__('3-forward').forward
-backward = __import__('5-backward').backward
+
+def forward(Observation, Emission, Transition, Initial):
+    """ performs the forward algorithm for a hidden markov model
+
+    Arguments:
+        Observation: np arr (T,) index of observation(s)
+        Emission: np arr (N, M) emission prob of specific observation
+            given hidden state
+        Transition: np arr (N, N) transition probabilities
+        Initial: np arr (N, 1) prob of starting in specific hidden state
+
+    Returns:
+        P, F, or None, None on failure
+            P: likelihood of the observations given the model
+            F: np arr (N, T) forward path probabilities
+    """
+    if type(Observation) is not np.ndarray or Observation.ndim != 1:
+        return None, None
+    if type(Emission) is not np.ndarray or Emission.ndim != 2:
+        return None, None
+    if type(Transition) is not np.ndarray or Transition.ndim != 2:
+        return None, None
+    if type(Initial) is not np.ndarray or Initial.ndim != 2:
+        return None, None
+
+    N, M = Emission.shape
+    if Transition.shape != (N, N):
+        return None, None
+    if Initial.shape != (N, 1):
+        return None, None
+
+    T = Observation.shape[0]
+
+    # initialize F
+    F = np.zeros((N, T))
+    F[:, 0] = Initial.T * Emission.T[Observation[0]]
+
+    for t in range(1, T):
+        F[:, t] = np.matmul(F[:, t - 1], Transition) *\
+            Emission.T[Observation[t]]
+
+    P = np.sum(F[:, T - 1])
+
+    return P, F
+
+
+def backward(Observation, Emission, Transition, Initial):
+    """ performs the backward algorithm for a hidden markov model
+
+    Arguments:
+        Observation: np arr (T,) index of observation(s)
+        Emission: np arr (N, M) emission prob of specific observation
+            given hidden state
+        Transition: np arr (N, N) transition probabilities
+        Initial: np arr (N, 1) prob of starting in specific hidden state
+
+    Returns:
+        P, B, or None, None on failure
+            P: likelihood of the observations given the model
+            B: np arr (N, T) containing the backward path probabilities
+                B[i, j] is the probability of generating the future
+                observations from hidden state i at time j
+    """
+    if type(Observation) is not np.ndarray or Observation.ndim != 1:
+        return None, None
+    if type(Emission) is not np.ndarray or Emission.ndim != 2:
+        return None, None
+    if type(Transition) is not np.ndarray or Transition.ndim != 2:
+        return None, None
+    if type(Initial) is not np.ndarray or Initial.ndim != 2:
+        return None, None
+
+    N, M = Emission.shape
+    if Transition.shape != (N, N):
+        return None, None
+    if Initial.shape != (N, 1):
+        return None, None
+
+    T = Observation.shape[0]
+
+    # initialization of Beta
+    B = np.zeros(shape=(N, T))
+    B.T[T - 1] = 1
+
+    for t in range(T - 2, -1, -1):
+        for s in range(N):
+            B[s, t] = (B.T[t + 1] * Transition[s, :] *
+                       Emission.T[Observation[t + 1]]).sum()
+
+    P = np.sum(Initial.T * Emission.T[Observation[0]] * B.T[0])
+
+    return P, B
 
 
 def baum_welch(Observations, Transition, Emission, Initial, iterations=1000):
