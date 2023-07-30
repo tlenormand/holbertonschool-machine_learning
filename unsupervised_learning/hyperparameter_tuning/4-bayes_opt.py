05c38a9c90e3ab86e1fea3e551ee5a2c92d7d7c8
tlenormand <lenormand.thomas76110@gmail.com>
2023-07-30 13:42:33 +0200

    fix: pycodestyle

--- unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
@@ -8,8 +8,7 @@ GP = __import__('2-gp').GaussianProcess
 
 
 class BayesianOptimization:
-    """ performs Bayesian optimization on a noiseless 1D Gaussian process """
-    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1, sigma_f=1,
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples, ll=1, sigma_f=1,
                  xsi=0.01, minimize=True):
         """ constructor
 
@@ -28,73 +27,7 @@ class BayesianOptimization:
                 performed for minimization (True) or maximization (False)
         """
         self.f = f
-        self.gp = GP(X_init, Y_init, l, sigma_f)
-        self.X_s = np.linspace(bounds[0], bounds[1], ac_samples).reshape(-1, 1)
-        self.xsi = xsi
-        self.minimize = minimize
-
-    #!/usr/bin/env python3
-""" Bayesian Optimization """
-
-import numpy as np
-
-GP = __import__('2-gp').GaussianProcess
-
-
-class BayesianOptimization:
-    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1, sigma_f=1,
-                 xsi=0.01, minimize=True):
-        """ constructor
-
-        Arguments:
-            f: black-box function to be optimized
-            X_init: np arr (t, 1) inputs sampled with black-box function
-            Y_init: np arr (t, 1) outputs of black-box function for each input
-            bounds: tuple (min, max) representing bounds of the space to
-                look for the optimal point
-            ac_samples: number of samples that should be analyzed during
-                acquisition
-            l: length parameter for kernel
-            sigma_f: standard deviation given to output of black-box function
-            xsi: exploration-exploitation factor for acquisition
-            minimize: bool determining whether optimization should be
-                performed for minimization (True) or maximization (False)
-        """
-        self.f = f
-        self.gp = GP(X_init, Y_init, l, sigma_f)
-        self.X_s = np.linspace(bounds[0], bounds[1], ac_samples).reshape(-1, 1)
-        self.xsi = xsi
-        self.minimize = minimize
-
-    #!/usr/bin/env python3
-""" Bayesian Optimization """
-
-import numpy as np
-
-GP = __import__('2-gp').GaussianProcess
-
-
-class BayesianOptimization:
-    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1, sigma_f=1,
-                 xsi=0.01, minimize=True):
-        """ constructor
-
-        Arguments:
-            f: black-box function to be optimized
-            X_init: np arr (t, 1) inputs sampled with black-box function
-            Y_init: np arr (t, 1) outputs of black-box function for each input
-            bounds: tuple (min, max) representing bounds of the space to
-                look for the optimal point
-            ac_samples: number of samples that should be analyzed during
-                acquisition
-            l: length parameter for kernel
-            sigma_f: standard deviation given to output of black-box function
-            xsi: exploration-exploitation factor for acquisition
-            minimize: bool determining whether optimization should be
-                performed for minimization (True) or maximization (False)
-        """
-        self.f = f
-        self.gp = GP(X_init, Y_init, l, sigma_f)
+        self.gp = GP(X_init, Y_init, ll, sigma_f)
         self.X_s = np.linspace(bounds[0], bounds[1], ac_samples).reshape(-1, 1)
         self.xsi = xsi
         self.minimize = minimize
