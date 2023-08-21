ecf14fad9631aee901f961d1e254d56f99f1efb2
tlenormand <lenormand.thomas76110@gmail.com>
2023-08-21 14:41:09 +0200

    feat: new

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -1,26 +1,12 @@
 #!/usr/bin/env python3
 """ Bag Of Words """
 
+from sklearn.feature_extraction.text import CountVectorizer
+
 
 def bag_of_words(sentences, vocab=None):
     """ creates a bag of words embedding matrix """
-    if vocab is None:
-        vocab = []
-        for sentence in sentences:
-            for word in sentence.split():
-                if word not in vocab:
-                    vocab.append(word)
-        vocab.sort()
+    vectorizer = CountVectorizer(vocabulary=vocab)
+    X = vectorizer.fit_transform(sentences)
 
-    array = []
-
-    for sentence in sentences:
-        temp = []
-        for word in vocab:
-            if word in sentence:
-                temp.append(1)
-            else:
-                temp.append(0)
-        array.append(temp)
-
-    return array, vocab
+    return X.toarray(), vectorizer.get_feature_names()
